f3b3e6fc353a46a9cf1f5c05915a1651b4651996
jinzhifeng <jin-zhifeng@hotmail.com>
2015-10-14 17:27:51 +0800

    Update 1628.c

--- 1628.c
+++ 1628.c
@@ -2,38 +2,38 @@
 /*  QQ356290462            */
 #include "1628.h"
 
-uint8_t  DispSegCode[]= {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};	 //共阴段码
+uint8_t  DispSegCode[]= {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};	 //¹²Òõ¶ÎÂë
 
 void SM1628_WriteOneByte(uint8_t WriteByte)
 {
 	uint8_t n;
 	for(n = 0; n < 8; n++)
 	{
-		SM1628_CLK_L();
+		SM1628_CLK_CLR();
 		if( WriteByte & 0x01 )
-			SM1628_DIO_H();
+			SM1628_DIO_SET();
 		else
-			SM1628_DIO_L();
-		SM1628_CLK_H();
+			SM1628_DIO_CLR();
+		SM1628_CLK_SET();
 		WriteByte >>= 1;
 	}
-	SM1628_DIO_H();
+	SM1628_DIO_SET();
 }
 
 void SM1628_SendCommand(uint8_t SendCommand)
 {
-	SM1628_STB_H();
+	SM1628_STB_SET();
 //	Delay_ms(1) ;
-	SM1628_STB_L();
+	SM1628_STB_CLR();
 	SM1628_WriteOneByte( SendCommand );
 
 }
 
 void SM1628_SendData(uint8_t Addr, uint8_t Data)
 {
-	SM1628_STB_H();
+	SM1628_STB_SET();
 //	Delay_ms(1) ;
-	SM1628_STB_L();
+	SM1628_STB_CLR();
 	SM1628_WriteOneByte( Addr | 0xC0 );
 	SM1628_WriteOneByte( Data );
 }
@@ -50,9 +50,9 @@ void SM1628_Display(uint8_t Data1, uint8_t Data2, uint8_t Data3, uint8_t Data4, 
 
 void SM1628_Init(void)
 {
-	SM1628_DIO_H();
-	SM1628_CLK_H();
-	SM1628_STB_H();
+	SM1628_DIO_SET();
+	SM1628_CLK_SET();
+	SM1628_STB_SET();
 	SM1628_SendCommand(0x00);
 //	SM1628_SendCommand(0x88);
 	printf("SM1628 Init ok...\r\n");
