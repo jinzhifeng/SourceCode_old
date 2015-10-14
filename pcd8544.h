713010569998d207164d55eccad4f6ec7a1ea7aa
jinzhifeng <jin-zhifeng@hotmail.com>
2015-10-14 17:37:15 +0800

    Update pcd8544.h

--- pcd8544.h
+++ pcd8544.h
@@ -6,23 +6,23 @@
 #include "stm32f4xx_hal.h"
 
 
-#define Lcd_Pcd8544_SDIN_H()						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_SET)
-#define Lcd_Pcd8544_SDIN_L() 						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_RESET)
+#define Lcd_Pcd8544_SDIN_SET()						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_SET)
+#define Lcd_Pcd8544_SDIN_CLR() 						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, GPIO_PIN_RESET)
 
-#define Lcd_Pcd8544_SCLK_H()						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_1, GPIO_PIN_SET)
-#define Lcd_Pcd8544_SCLK_L() 						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_1, GPIO_PIN_RESET)
+#define Lcd_Pcd8544_SCLK_SET()						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_1, GPIO_PIN_SET)
+#define Lcd_Pcd8544_SCLK_CLR() 						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_1, GPIO_PIN_RESET)
 
-#define Lcd_Pcd8544_DC_H()							HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, GPIO_PIN_SET)
-#define Lcd_Pcd8544_DC_L() 							HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, GPIO_PIN_RESET)
+#define Lcd_Pcd8544_DC_SET()							HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, GPIO_PIN_SET)
+#define Lcd_Pcd8544_DC_CLR() 							HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, GPIO_PIN_RESET)
 
-#define Lcd_Pcd8544_SCE_H()							HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, GPIO_PIN_SET)
-#define Lcd_Pcd8544_SCE_L() 						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, GPIO_PIN_RESET)
+#define Lcd_Pcd8544_SCE_SET()							HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, GPIO_PIN_SET)
+#define Lcd_Pcd8544_SCE_CLR() 						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, GPIO_PIN_RESET)
 
-#define Lcd_Pcd8544_RST_H()							HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, GPIO_PIN_SET)
-#define Lcd_Pcd8544_RST_L() 						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, GPIO_PIN_RESET)
+#define Lcd_Pcd8544_RST_SET()							HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, GPIO_PIN_SET)
+#define Lcd_Pcd8544_RST_CLR() 						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, GPIO_PIN_RESET)
 
-#define Lcd_Pcd8544_BACKLIGHT_H()				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, GPIO_PIN_SET)
-#define Lcd_Pcd8544_BACKLIGHT_L() 			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, GPIO_PIN_RESET)
+#define Lcd_Pcd8544_BACKLIGHT_SET()				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, GPIO_PIN_SET)
+#define Lcd_Pcd8544_BACKLIGHT_CLR() 			HAL_GPIO_WritePin(GPIOD, GPIO_PIN_4, GPIO_PIN_RESET)
 
 void Lcd_Pcd8544_WriteByte(uint8_t WriteByte, uint8_t CommandOrDataSelect);
 void Lcd_Pcd8544_Init(void);
@@ -32,6 +32,7 @@ void Lcd_Pcd8544_SetPos(uint16_t Xpos, uint16_t Ypos);
 void Lcd_Pcd8544_PutEnglishChar(uint8_t EnglishChar);
 void Lcd_Pcd8544_PutEnglishString(uint16_t X, uint16_t Y, uint8_t *EnglishString);
 #endif
+
 
 /*  github.com/jinzhifeng  */
 /*  QQ356290462            */
